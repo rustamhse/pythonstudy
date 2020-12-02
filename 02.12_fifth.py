b4fe1fa793125bcf2d51bd6728593f210b74a24d
rustamhse <74643940+rustamhse@users.noreply.github.com>
2020-12-02 11:58:15 +0300

    Update 02.12_fifth.py

--- 02.12_fifth.py
+++ 02.12_fifth.py
@@ -1,7 +1,3 @@
-a = ["Yurii Grinkov 18", "Maxim Grinkov 22", "Elizaveta Grinkova 17", "Natalia Grinkova 45"]
-a = sorted(a, key = lambda x: int(x[-2]))
-answer = a[0]
-for i in range(0, len(a) - 1):
-  if a[i] < answer:
-    answer = a[i]
-print(answer)
+l = ["Yurii Grinkov 18", "Maxim Grinkov 22", "Elizaveta Grinkova 17", "Natalia Grinkova 45"]
+print(sorted(l, key = lambda x: int(x[-2:])))
+print(sorted(l, key = lambda x: int(x[-2:]))[0])
